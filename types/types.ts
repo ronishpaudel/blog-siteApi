4921bf56706af4347c8cf2f872af869e08a022b0
robuGdada <paudelronish@gmail.com>
2023-08-02 21:48:44 +0545

    add interface for typesafety

---
+++ types/types.ts
@@ -0,0 +1,11 @@
+export interface IUserData {
+  fname: string;
+  lname: string;
+  phoneNumber: number;
+  companyName: string;
+  password: string;
+  email: string;
+}
+export interface IUserVerified {
+  isVerified: boolean;
+}
