08fe9bfe681ef8f99fc1a9ea81f952032510f30f
robuGdada <paudelronish@gmail.com>
2023-08-02 21:47:04 +0545

    chore

--- resources/users/user.controller.ts
+++ resources/users/user.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { userRepo } from "./user.repo";
-import bcrypt, { genSalt } from "bcrypt";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 //get | query
 const getAll = async (req: Request, res: Response) => {
@@ -27,6 +28,20 @@ const getAll = async (req: Request, res: Response) => {
     res.status(400).json({ e });
   }
 };
+// const getEmail = async (req: Request, res: Response) => {
+//   const { email } = req.body;
+//   try {
+//     const userEmail = {
+//       email: email,
+//     };
+//     const users = await userRepo.getEmail(userEmail);
+//     console.log({ users });
+//     res.json(users);
+//   } catch (e) {
+//     console.log(e);
+//     res.status(400).json({ e });
+//   }
+// };
 
 //post | mutation
 const createUser = async (req: Request, res: Response) => {
@@ -43,16 +58,44 @@ const createUser = async (req: Request, res: Response) => {
       companyName: companyName,
       phoneNumber: phoneNumber,
     };
-
     const user = await userRepo.createUser(userData);
     console.log(user);
+    const { sign } = jwt;
+    const accessToken = sign(
+      {
+        email: email,
+        iat: Math.floor(Date.now() / 1000) - 30,
+      },
+      process.env.JWT_SECRET_KEY!
+    );
+    const link = `${process.env.BLOG_PAGE}?token=${accessToken}`;
+    console.log({ link });
     res.json(user);
   } catch (e) {
     console.error(e);
     res.status(400).json({ error: "Failed to create user." });
   }
 };
+
+// const updateUserVerified = async (req: Request, res: Response) => {
+//   const { email, isVerified } = req.body;
+//   try {
+//     const userEmail = {
+//       email: email,
+//     };
+//     const userIsVerified = {
+//       isVerified: isVerified === true,
+//     };
+//     const updateUser = await userRepo.updateUserVerified(
+//       userEmail,
+//       userIsVerified
+//     );
+//     res.json(updateUser);
+//   } catch (e) {}
+// };
+
 export const userController = {
   createUser,
   getAll,
+  // updateUserVerified,
 };
