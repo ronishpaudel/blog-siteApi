f552bc250202b6bdfc03950b536287c38e241a57
robuGdada <paudelronish@gmail.com>
2023-09-08 15:24:55 +0545

    redirect using slug instead of id

--- blogSite-server/blog.route.ts
+++ blogSite-server/blog.route.ts
@@ -1,16 +1,19 @@
 import express from "express";
 import { blogController } from ".";
-import { checkJwt } from "../resources/users/user.controller";
+import { verifyUser } from "../resources/users/user.controller";
 
 const blogRoute = express.Router();
 
 blogRoute.get("/blogs", async (req, res) => {
   await blogController.getAll(req, res);
 });
-blogRoute.get("/blogs/:id", async (req, res) => {
-  await blogController.getBlogById(req, res);
+// blogRoute.get("/blogs/:id", async (req, res) => {
+//   await blogController.getBlogById(req, res);
+// });
+blogRoute.get("/blogs/:slug", async (req, res) => {
+  await blogController.getBlogBySlug(req, res);
 });
-blogRoute.post("/blogs", checkJwt, async (req, res) => {
+blogRoute.post("/blogs", verifyUser, async (req, res) => {
   await blogController.createBlog(req, res);
 });
 export { blogRoute };
