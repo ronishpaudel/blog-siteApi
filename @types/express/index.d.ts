a3e21664df9c6b7a32bdd35e60337b8f9e0d6499
robuGdada <paudelronish@gmail.com>
2024-08-12 21:02:52 +0545

    commented err solution if generated while creating own express type

--- @types/express/index.d.ts
+++ @types/express/index.d.ts
@@ -1,5 +1,10 @@
 import { User } from "@prisma/client";
 
+//add this in tsconfig.json file to resolve error of authUser property not found in request object
+// "typeRoots": [
+//   "@types",
+//   "node_modules/@types"
+// ]
 declare global {
   namespace Express {
     interface Request {
